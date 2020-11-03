53b270becc97e23be997fe14322c327ed61e834a
chexov <chex0ff@inbox.ru>
2020-11-04 01:32:32 +0400

    realized function sortStrings

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,13 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+  return [...arr].sort((a, b) => {
+    switch (param) {
+        case 'desc':
+          return b.localeCompare(a, ['ru', 'en'], {caseFirst: "upper"})            
+        case 'asc':
+        default:
+          return a.localeCompare(b, ['ru', 'en'], {caseFirst: "upper"})
+    }
+  })
 }
