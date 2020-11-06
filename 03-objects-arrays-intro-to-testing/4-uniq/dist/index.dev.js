3f8470b62235e750c29644c92d34e1d1f01daebc
chexov <chex0ff@inbox.ru>
2020-11-06 23:09:30 +0400

    first implementation of the uniq function on the fly

---
+++ 03-objects-arrays-intro-to-testing/4-uniq/dist/index.dev.js
@@ -0,0 +1,23 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.uniq = uniq;
+
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
+
+function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
+
+/**
+ * uniq - returns array of uniq values:
+ * @param {*[]} arr - the array of primitive values
+ * @returns {*[]} - the new array with uniq values
+ */
+function uniq(arr) {
+  return _toConsumableArray(new Set(arr).values());
+}
