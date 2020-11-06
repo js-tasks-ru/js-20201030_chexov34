c527ada2ede20ef46f3d851af5d0f7879b283920
chexov <chex0ff@inbox.ru>
2020-11-06 22:57:53 +0400

    first implementation of the createGetter function on the fly

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,16 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  const arrParams = path.split('.')
+  return obj => {
+    let copyObj = {...obj}
+    for (let k of arrParams) {
+      if (Object.keys(copyObj).includes(k)) {
+        copyObj = copyObj[k]
+      } else {
+        return undefined
+      }
+    }
+    return copyObj
+  }
 }
